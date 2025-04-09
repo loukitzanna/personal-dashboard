5b35c2183cef2da2d348905ed62bca3673dfb4a2
Anna Loukianova <loukitzanna@gmail.com>
2025-04-08 22:38:34 -0700

    add theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import { HeroUIProvider } from "@heroui/react";
 import { ApolloProvider } from '@apollo/client';
+import { HeroUIProvider, Switch } from '@heroui/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import client from './graphql/client';
 import Dashboard from './components/Dashboard';
+import ThemeSwitch from './components/ThemeSwitcher';
 import './index.css';
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <HeroUIProvider>
-        <div className="min-h-screen bg-gray-50">
-          <header className="bg-white shadow">
-            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold text-gray-900">Welcome to your Dashboard!</h1>
-            </div>
-          </header>
-          <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <Dashboard />
-          </main>
-        </div>
-      </HeroUIProvider>
-    </ApolloProvider>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <HeroUIProvider>
+                <NextThemesProvider attribute='class' defaultTheme='dark'>
+                    <div className='min-h-screen bg-gray-50 dark:bg-black'>
+                        <header className='bg-white dark:bg-black shadow'>
+                            <div className='max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center'>
+                                <h1 className='text-3xl font-bold text-gray-900 dark:text-white'>
+                                    Welcome to your Dashboard!
+                                </h1>
+                                <ThemeSwitch />
+                            </div>
+                        </header>
+                        <main className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
+                            <Dashboard />
+                        </main>
+                    </div>
+                </NextThemesProvider>
+            </HeroUIProvider>
+        </ApolloProvider>
+    );
 }
 
 export default App;
